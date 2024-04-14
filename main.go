78adf9fe17a85a690408117a1a1fcf0e36e9696b
erikvim <erikdavid.hedman21@outlook.com>
2024-04-13 22:56:10 -0400

    Created a file to practice using user input and coverting types

--- main.go
+++ main.go
@@ -1,32 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
-
-// since the variable starts with a lowercase a, it is private and only can be accessed within this file.
-// If it had an uppercase A at the beginning, it would be public, and it would be accessible within any file within this directory
-const aConst int = 64
 
 func main() {
 
-	var aString string = "This is Go!"
+	//Initializing the variable with bufio.NewReader(), this function requires a parameter indicating where the data is going to come from, in this case, it will come from os.Stdin.  This stands for Standard Input, what the user types on the command line.
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println(aString)
+	//Displaying a prompt to the reader:
+	fmt.Print("Enter text: ")
 
-	var defaultInt int
-	fmt.Println(defaultInt)
-	fmt.Printf("This variable's type is %T\n", defaultInt)
-
-	var anotherInt = 53
-	fmt.Println(anotherInt)
-	fmt.Printf("This variable's type is %T\n", anotherInt)
-
-	//another way of implicitly typing a variable is by using :=
-	//You can only use the := to implicitly type variables within a function.  Any variable declared outsite of a function must use the var keyword.
-	//Same for constants, you must use the const keyword
-	myString := "This is also a string"
-	fmt.Println(myString)
-	fmt.Printf("This variable's type is %T\n", myString)
-
+	//Now, we want the application to pause and let the user type something in.
+	//We are declaring two variables, input for the input that the user will be typing into the console, and _, for error handling.  _ means that we want to ignore that variable.
+	//We are initializing the variables with reader.ReadString(), this function accepts a single character, the deliminator, which tells the ReadString function when to stop receiving input.  In this case, we are inputing the argument of '\n' to print a new line as a character
+	//The ReadString function knows to stop reading the string that was entered after the new string that gets printed by the Println().  Print() doesn't print a new line after the argument is passed in.  But, Println does.
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered:", input)
 }
