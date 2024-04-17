d2895e1c56c3f3087708428a3df4979d567f7fa4
erikvim <erikdavid.hedman21@outlook.com>
2024-04-17 14:49:04 -0400

    Created a branch for learning the data type struct and how to access the properties within it

--- main.go
+++ main.go
@@ -4,29 +4,31 @@ import (
 	"fmt"
 )
 
-// since the variable starts with a lowercase a, it is private and only can be accessed within this file.
-// If it had an uppercase A at the beginning, it would be public, and it would be accessible within any file within this directory
-const aConst int = 64
-
 func main() {
+	//This is creating an instance of the struct Dog.  This is like defining a constructor in other languages, but you are not calling a method,
+	//you are defining an object, so we do not use (), we use {}
+	//Pass in the values of the object in the order they are declared:
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	//to see a structs values for debugging purposes, use the Printf()
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
 
-	var aString string = "This is Go!"
+	//if you want to change the values of the fields in the struct:
 
-	fmt.Println(aString)
+	poodle.Weight = 9
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
 
-	var defaultInt int
-	fmt.Println(defaultInt)
-	fmt.Printf("This variable's type is %T\n", defaultInt)
+}
 
-	var anotherInt = 53
-	fmt.Println(anotherInt)
-	fmt.Printf("This variable's type is %T\n", anotherInt)
+//If you name the type with an upper case initial letter, it can be used globally, or in Go terms, it is exported
+//If you name the type with a lower case initial letter, it is considered private, or non exported
 
-	//another way of implicitly typing a variable is by using :=
-	//You can only use the := to implicitly type variables within a function.  Any variable declared outsite of a function must use the var keyword.
-	//Same for constants, you must use the const keyword
-	myString := "This is also a string"
-	fmt.Println(myString)
-	fmt.Printf("This variable's type is %T\n", myString)
+//The same goes for the properties (fields) of the struct
 
+// Dog is a struct
+type Dog struct {
+	Breed  string
+	Weight int
+	// each value of a struct is technically a field, you access the structs individual fields using the . operator
 }
