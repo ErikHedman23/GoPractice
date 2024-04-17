57ca3a12aa7ca5e383717438bf1f503d97f74a00
erikvim <erikdavid.hedman21@outlook.com>
2024-04-17 12:31:22 -0400

    Created a branch for arrays

--- main.go
+++ main.go
@@ -4,29 +4,22 @@ import (
 	"fmt"
 )
 
-// since the variable starts with a lowercase a, it is private and only can be accessed within this file.
-// If it had an uppercase A at the beginning, it would be public, and it would be accessible within any file within this directory
-const aConst int = 64
-
 func main() {
 
-	var aString string = "This is Go!"
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+	fmt.Println(colors[0])
 
-	fmt.Println(aString)
+	var numbers = [5]int{5, 4, 3, 2, 1}
 
-	var defaultInt int
-	fmt.Println(defaultInt)
-	fmt.Printf("This variable's type is %T\n", defaultInt)
+	fmt.Println(numbers)
 
-	var anotherInt = 53
-	fmt.Println(anotherInt)
-	fmt.Printf("This variable's type is %T\n", anotherInt)
+	//print the count of the items in the array:
 
-	//another way of implicitly typing a variable is by using :=
-	//You can only use the := to implicitly type variables within a function.  Any variable declared outsite of a function must use the var keyword.
-	//Same for constants, you must use the const keyword
-	myString := "This is also a string"
-	fmt.Println(myString)
-	fmt.Printf("This variable's type is %T\n", myString)
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of numbers", len(numbers))
 
 }
