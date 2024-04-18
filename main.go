75a0cfbecaacbc6cadbca341fa67e994211be011
erikvim <erikdavid.hedman21@outlook.com>
2024-04-18 14:45:22 -0400

    Created a branch for learning for loops

--- main.go
+++ main.go
@@ -4,29 +4,74 @@ import (
 	"fmt"
 )
 
-// since the variable starts with a lowercase a, it is private and only can be accessed within this file.
-// If it had an uppercase A at the beginning, it would be public, and it would be accessible within any file within this directory
-const aConst int = 64
-
 func main() {
 
-	var aString string = "This is Go!"
+	colors := []string{"Red", "Green", "Blue"}
+	//Go has several iterations of the for loop.  It does not have a while loop, but the for loop can act as a while loop when needed.
+	//To perform the traditional C style for loop with 3 declarations:
+	//Here, you are initializing a local variable of i for the index of the slice starting at 0
+	//Then, you are stating that you will continue the loop till you get to the end of the length of the slice of i
+	//Finally, you are incrementing by 1 index in the slice each time using the incrementor of i++
+	//For each time the loop iterates over the slice, we print to the console the index of colors using colors[i]
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+	//This style of for loop is the exact same as the one above, it is just easier to read and more manageable:
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
 
-	fmt.Println(aString)
+	/*The next way of using a for loop is like how you would use a foreach loop, although slightly different;
+		You will be returning two variables from the range from a comma delimited list.
+		The first variable will represent the index, and if you want to ignore the index, you can simply put the _ to ignore it.
+		The second variable will be the value of the index
+	It works just like how it does in C#, only you also get to return the index if you wish:
+	for(int color in colors)
+	{
 
-	var defaultInt int
-	fmt.Println(defaultInt)
-	fmt.Printf("This variable's type is %T\n", defaultInt)
+	}
+	*/
+	for _, color := range colors {
+		fmt.Println(color)
+	}
 
-	var anotherInt = 53
-	fmt.Println(anotherInt)
-	fmt.Printf("This variable's type is %T\n", anotherInt)
+	//In Go, there is no while loop, but you can initialize a counter or a boolean expression to act as your condition for how long the loop
+	//iterates for:
+	value := 1
+	for value < 10 {
+		fmt.Println("Value:", value)
+		value++
+	}
 
-	//another way of implicitly typing a variable is by using :=
-	//You can only use the := to implicitly type variables within a function.  Any variable declared outsite of a function must use the var keyword.
-	//Same for constants, you must use the const keyword
-	myString := "This is also a string"
-	fmt.Println(myString)
-	fmt.Printf("This variable's type is %T\n", myString)
+	//break and continue are used in Go for for loops.  break means to go to the end of the current code block, this works for both for and
+	//switch statements.  continue means to start back at the beginning of the current code block
+	//There is also the goto statement with labels.  You can add labels to your code and jump to your label in your code with a goto or a break
+	//statement.
+	//You can do the same thing with this by using a break, you just wouldn't create a label, you would just say break, and then once the code hops out
+	//of the for loop, you can print the "End of program" string afterwards
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println("Sum:", sum)
+		if sum > 200 {
+			goto theEnd
+		}
+	}
 
+theEnd:
+	fmt.Println("End of program")
+
+	//Here is an example of using a continue in a for loop:
+
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			// Skip even numbers
+			continue
+		}
+		fmt.Println("Odd number:", i)
+	}
+
+	/*
+		In this example, the loop iterates from 0 to 9. When i is an even number, the continue statement is executed, which skips the rest of the loop body and continues with the next iteration of the loop. As a result, only odd numbers are printed.
+	*/
 }
