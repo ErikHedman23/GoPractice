d9ced4053d62bccdf85c972666dafaa473367a24
erikvim <erikdavid.hedman21@outlook.com>
2024-04-19 09:38:10 -0400

    Created some methods for my Dog type

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	//This is creating an instance of the struct Dog.  This is like defining a constructor in other languages, but you are not calling a method,
 	//you are defining an object, so we do not use (), we use {}
 	//Pass in the values of the object in the order they are declared:
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "Bark!"}
 	fmt.Println(poodle)
 	//to see a structs values for debugging purposes, use the Printf()
 	fmt.Printf("%+v\n", poodle)
@@ -18,6 +18,10 @@ func main() {
 
 	poodle.Weight = 9
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Speak()
+
+	poodle.SpeakThreeTimes()
 
 }
 
@@ -30,5 +34,23 @@ func main() {
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
 	// each value of a struct is technically a field, you access the structs individual fields using the . operator
+
+}
+
+//to create methods (functions for structs) for types, you do so like this: define your function, and pass in the receiver.  You pass inside parens
+//the identifier (in this case d) and then the type:
+
+// You are now able to access the Sound field for Dog by using your identifying reference:
+// Speak is how the dog speaks
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+// SpeakThreeTimes is how the dog speaks three times
+func (d Dog) SpeakThreeTimes() {
+	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+	//The same as Printf, but, instead of outputting to the console, it returns a string
+	fmt.Println(d.Sound)
 }
